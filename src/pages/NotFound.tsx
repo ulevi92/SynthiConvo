5c3cfbba16507841eb01c5fe362015903670cb53
Uria <ulevi92@gmail.com>
2023-07-18 18:21:54 +0300

    modified/ NotFound page to auto navigate to "/"

--- src/pages/NotFound.tsx
+++ src/pages/NotFound.tsx
@@ -1,4 +1,15 @@
+import { useEffect } from "react";
+import { useNavigate } from "react-router-dom";
+
 const NotFound = () => {
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    const timer = setInterval(() => navigate("/"), 2000);
+
+    return () => clearInterval(timer);
+  }, []);
+
   return <>Not Found</>;
 };
 
