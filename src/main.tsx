bbcf11bce3fa2654e89b0482281cf7b047b9407e
Uria <ulevi92@gmail.com>
2023-12-09 16:38:04 +0200

    removed/ navbar as no longer needed

--- src/main.tsx
+++ src/main.tsx
@@ -7,7 +7,7 @@ import "./styles/global.scss";
 import { Provider } from "react-redux";
 import store from "./redux/store";
 import { BrowserRouter } from "react-router-dom";
-import AppNavbar from "./components/layouts/navbar/AppNavbar";
+
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import { ThemeProvider } from "react-bootstrap";
@@ -27,7 +27,6 @@ ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
     <Provider store={store}>
       <BrowserRouter>
         <div className='d-flex vh-100 flex-column'>
-          <AppNavbar />
           <div className='flex-grow-1'>
             <App />
           </div>
