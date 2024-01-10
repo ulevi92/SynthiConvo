096e6a2a16b20408fff1b82d78472acd24a55169
Uria <ulevi92@gmail.com>
2024-01-10 12:57:36 +0200

    fixed/ style bug created my mistake

--- src/components/chat/ChatWindow.tsx
+++ src/components/chat/ChatWindow.tsx
@@ -5,12 +5,12 @@ const ChatWindow = () => {
   const gridColClassName = "p-0";
 
   return (
-    <div className='d-flex position-relative'>
+    <div className='d-flex flex-column position-relative'>
       <div style={{ maxHeight: "85vh" }} className='overflow-y-auto'>
         <ChatLog />
       </div>
 
-      <div className='align-self-end'>
+      <div>
         <ChatForm />
       </div>
     </div>
