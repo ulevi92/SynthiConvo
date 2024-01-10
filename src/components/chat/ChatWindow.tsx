8908133f6117d025e7b1eb39ff89bac6f613ad12
Uria <ulevi92@gmail.com>
2024-01-10 13:03:14 +0200

    fixed/ style bug when you clear conversation

--- src/components/chat/ChatWindow.tsx
+++ src/components/chat/ChatWindow.tsx
@@ -5,12 +5,12 @@ const ChatWindow = () => {
   const gridColClassName = "p-0";
 
   return (
-    <div className='d-flex flex-column position-relative'>
+    <div className='d-flex flex-column flex-grow-1 position-relative justify-content-between'>
       <div style={{ maxHeight: "85vh" }} className='overflow-y-auto'>
         <ChatLog />
       </div>
 
-      <div>
+      <div className=''>
         <ChatForm />
       </div>
     </div>
